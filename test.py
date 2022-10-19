5158c539000323031ea150b3dcffba8200472c7f
CarrelV <vin100.c@hotmail.com>
2022-10-19 15:05:12 +0200

    i2t and t2i flickr

--- test.py
+++ test.py
@@ -6,15 +6,14 @@ from tqdm import tqdm
 from models import CLIPModel
 import config as CFG
 from tokenizer import get_tokenizer
-from dataloader import transform_test
+from dataloader import transform_test,get_dataloader
 from utils import read_imagenet_class
 
-def test_0shot():
+def test():
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
-    ds = ImageNetV2Dataset(transform=transform_test)
-    test_loader = DataLoader(ds, batch_size=CFG.test_batch_size, num_workers=CFG.num_workers)
+    
 
     tokenizer = get_tokenizer(CFG.text_model_name)
 
@@ -27,7 +26,17 @@ def test_0shot():
     model.text_projection.load_state_dict(checkpoint_text)
 
 
-    
+    imagenet_0shot(model=model,tokenizer=tokenizer,device=device)
+
+    flickr_retrieval(model=model,tokenizer=tokenizer,device=device)
+
+######################## IMAGENET 0 SHOT ###############
+
+
+def imagenet_0shot(model,tokenizer,device):
+
+    ds = ImageNetV2Dataset(transform=transform_test)
+    test_loader = DataLoader(ds, batch_size=CFG.test_batch_size, num_workers=CFG.num_workers)
 
     imagenet_prompt ='a photo of a {}.'
 
@@ -55,8 +64,11 @@ def test_0shot():
     top1 = (top1 / n) * 100
     top5 = (top5 / n) * 100 
 
+    print("0 Shot classification on ImageNetV2:")
     print(f"Top-1 accuracy: {top1:.2f}")
     print(f"Top-5 accuracy: {top5:.2f}")
+
+
 
 def compute_text_weight_zeroshot(model,tokenizer,device,classnames, template):
     with torch.no_grad():
@@ -82,9 +94,96 @@ def accuracy(output, target, topk=(1,)):
     correct = pred.eq(target.view(1, -1).expand_as(pred))
     return [float(correct[:k].reshape(-1).float().sum(0, keepdim=True).cpu().numpy()) for k in topk]
 
+#########################  FLICKR I2T and T2I RETRIEVAL ########################
+
+def i2t(sim: torch.Tensor):
+    
+    order = sim.argsort()
+    
+    top1 = 0
+    top5 = 0
+    top10 = 0
+
+    for i in range(len(order[0])):
+        if i == order[i,0]:
+            top1 += 1
+        if i in order[i,0:5]:
+            top5 += 1
+        if i in order[i,:10]:
+            top10 += 1
+
+    return 100 * top1 / len(order[0]), 100 * top5 / len(order[0]) , 100 * top10 / len(order[0])
+
+def t2i(sim: torch.Tensor):
+    
+    sim = sim.T
+    order = sim.argsort()
+    
+    top1 = 0
+    top5 = 0
+    top10 = 0
+
+    for i in range(len(order[0])):
+        if i == order[i,0]:
+            top1 += 1
+        if i in order[i,0:5]:
+            top5 += 1
+        if i in order[i,:10]:
+            top10 += 1
+
+    return 100 * top1 / len(order[0]), 100 * top5 / len(order[0]) ,100 * top10 / len(order[0])
+
+
+def flickr_retrieval(model,tokenizer,device):
+
+
+    train_loader = get_dataloader(tokenizer=tokenizer,batch_size=CFG.batch_size,shuffle=False,num_workers=CFG.num_workers,split="test")
+
+    with torch.no_grad():
+
+        for batch in train_loader:
+
+            image = batch["image"].to(device)
+            text = {"input_ids": batch["input_ids"].to(device), "attention_mask": batch["attention_mask"].to(device)}
+
+            # compute image_features
+            image_features = model.encode_image(image)
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+
+            # compute text features
+            text_features = model.encode_text(text)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+
+            
+
+            similarities = image_features @ text_features.T
+
+            i2t_r1,i2t_r5,i2t_r10 = i2t(similarities)
+            t2i_r1,t2i_r5,t2i_r10 = t2i(similarities)
+
+            print("Image 2 Text Retrieval on Flickr30k:")
+            print(f"Top-1 accuracy: {i2t_r1:.2f}")
+            print(f"Top-5 accuracy: {i2t_r5:.2f}")
+            print(f"Top-10 accuracy: {i2t_r10:.2f}")
+
+            print("Text 2 Image Retrieval on Flickr30k:")
+            print(f"Top-1 accuracy: {t2i_r1:.2f}")
+            print(f"Top-5 accuracy: {t2i_r5:.2f}")
+            print(f"Top-10 accuracy: {t2i_r10:.2f}")
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
 
   
 
-    test_0shot()
+    test()
