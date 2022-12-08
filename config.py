8717ab4d079cd9d73750cfb76c31c3a19a3d0d96
CarrelV <vin100.c@hotmail.com>
2022-12-08 18:12:34 +0100

    test ape

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ gpu_number = 2
 #             Testing          #
 ################################
 
-configuration_to_test = "costly_baseline"
+configuration_to_test = "APE"
 
 weight_version = 1
 #############################################################################
