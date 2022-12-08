c4985c4fe75adf9f480dfced13f0348061cc8a2e
CarrelV <vin100.c@hotmail.com>
2022-12-08 18:09:36 +0100

    test good_baseline

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ gpu_number = 2
 #             Testing          #
 ################################
 
-configuration_to_test = "baseline"
+configuration_to_test = "good_baseline"
 
 weight_version = 1
 #############################################################################
