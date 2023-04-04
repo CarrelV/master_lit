8943e85117f2dec35dd227f4c04ec0232ace84a3
CarrelV <vin100.c@hotmail.com>
2023-04-04 12:20:12 +0200

    params

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ training_run_number = 1
 # 1024 when both backbone are frozen (baseline,good_baseline,APE)
 # 64 when both backbone are finetuned (bad_baseline)
 # 128 when only the text backbone is finetuned (costly_baseline,LiT,APE_LiT)
-batch_size = 512
+batch_size = 256
 
 warming_epochs = 20
 epochs = 300
