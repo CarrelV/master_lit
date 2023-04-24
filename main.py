5f25d0fad7b344c261a9d053e557f1927851f4e3
CarrelV <vin100.c@hotmail.com>
2023-04-24 20:45:50 +0200

    train base BERT on flickr30k

--- main.py
+++ main.py
@@ -48,11 +48,12 @@ def main(rank,world_size):
     
     if CFG.side_text_weights_copy:
         print("Starting copying the weights to the pruned side network")
-        importance_measure = compute_fisher(model, get_dataloader(dataset="mscoco",tokenizer=tokenizer,feature_extractor=feature_extractor,rank=rank,world_size=world_size,batch_size=1,shuffle=CFG.shuffle_train,num_workers=CFG.num_workers,split="train"), num_samples=CFG.samples_for_fisher)
+        importance_measure = compute_fisher(model, get_dataloader(dataset="flickr30k",tokenizer=tokenizer,feature_extractor=feature_extractor,rank=rank,world_size=world_size,batch_size=1,shuffle=CFG.shuffle_train,num_workers=CFG.num_workers,split="train"), num_samples=CFG.samples_for_fisher)
         print("fisher importance measure computed")
         model = modify_text_model_after_init(model,tokenizer,importance_measure)
         print("Finish copying the weights to the pruned side network")
 
+    importance_measure = None
     
 
     resume_model(model)
