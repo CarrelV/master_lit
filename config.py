0ca3bc1f3551064c5a0d5dbceba483db104ec4c5
CarrelV <vin100.c@hotmail.com>
2023-04-29 00:39:26 +0200

    train LST reduce ladder on flickr

--- config.py
+++ config.py
@@ -27,9 +27,9 @@ training_run_number = "flickr_redlad_4"
 # 64 when both backbone are finetuned (bad_baseline)
 # 128 when only the text backbone is finetuned (costly_baseline,LiT,APE_LiT)
 # 32 for BERT base uncased with LST
-batch_size = 512
+batch_size = 256
 
-test_batch_size = 256
+test_batch_size = 128
 
 # 20 / 300 on flickr
 # 5 / 50 on MSCOCO
