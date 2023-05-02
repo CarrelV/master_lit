3b55dadcfc9c315e1b28052aa5c0b9b3aa0812e4
CarrelV <vin100.c@hotmail.com>
2023-05-02 20:39:23 +0200

    test LST cut ladder on mscoco

--- config.py
+++ config.py
@@ -11,8 +11,8 @@
 #             Training         #
 ################################
 
-text_model_size = "medium"
-image_model_size = "medium"
+text_model_size = "small"
+image_model_size = "small"
 
 ## See at the end the different possibilities
 configuration = "LST"
@@ -22,7 +22,7 @@ dataset = "flickr30k"
 #dataset = "mscoco"
 
 # Increment if retraining the same configuration one more time
-training_run_number = "flickr_baseBERT_redlad4"
+training_run_number = "flickr_redlad_4"
 
 # 1024 when both backbone are frozen (baseline,good_baseline,APE)
 # 64 when both backbone are finetuned (bad_baseline)
@@ -54,7 +54,7 @@ sum_last_outputs = True
 
 configuration_to_test = "LST"
 
-weight_version = "flickr_baseBERT_redlad4"
+weight_version = "flickr_redlad_4"
 #############################################################################
 #                                                                           #
 #                            END MODIFICATION                               #
