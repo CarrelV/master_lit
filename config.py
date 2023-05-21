8c52236935a409c217b58e9ed7e794a77e607fdf
CarrelV <vin100.c@hotmail.com>
2023-05-21 12:15:43 +0200

    training e-3

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ testing = False
 dataset = "mscoco"
 
 # Increment if retraining the same configuration one more time
-training_run_number = "LST_Ismall_Tmedium_lr2"
+training_run_number = "LST_Ismall_Tmedium_lr3"
 
 # 1024 when both backbone are frozen (baseline,good_baseline,APE)
 # 64 when both backbone are finetuned (bad_baseline)
@@ -54,7 +54,7 @@ sum_last_outputs = True
 
 configuration_to_test = "LST"
 
-weight_version = "LST_Ismall_Tmedium_lr2"
+weight_version = "LST_Ismall_Tmedium_lr3"
 #############################################################################
 #                                                                           #
 #                            END MODIFICATION                               #
@@ -160,7 +160,7 @@ split = "train"
 # 
 
 image_encoder_lr = 5e-5
-text_encoder_lr = 1e-2
+text_encoder_lr = 1e-3
 image_head_lr = 1e-3
 text_head_lr = 1e-3
 
