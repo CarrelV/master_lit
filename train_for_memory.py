fdca2c4add7111366f3dcea796fae462aa0f31b5
CarrelV <vin100.c@hotmail.com>
2024-05-23 22:43:40 +0200

    memory benchmark

---
+++ train_for_memory.py
@@ -0,0 +1,176 @@
+import config as CFG
+from dataloader import get_local_dataloader
+from tokenizer import get_tokenizer,get_feature_extractor
+from models_CLIP import CLIPMoco, get_classic_model
+from losses import CLIPMoCOLoss
+from training_for_memory import train_one_epoch, valid_one_epoch,get_lr
+from utils import setup,cleanup
+from utils_models import modify_model_after_init,resume_model
+
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
+import wandb
+import torch
+from transformers.optimization import get_cosine_schedule_with_warmup
+from transformers import logging
+
+from fisher import compute_fisher
+
+from metric import imagenet_0shot,i2t_t2i_retrieval
+import csv
+data_to_save = []
+
+
+def main():
+    logging.set_verbosity_error()
+    
+
+    # setup the process groups
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    data_to_save.append(CFG.run_info)
+    data_to_save.append(CFG.text_tower_name)
+    data_to_save.append(CFG.text_head_name)
+    data_to_save.append(CFG.image_tower_name)
+    data_to_save.append(CFG.image_head_name)
+    # prepare the dataloader
+    
+    print("prepare the dataloader")
+
+    #print(f"Beginning memory: {torch.cuda.memory_allocated(rank)}")
+
+    tokenizer = get_tokenizer(CFG.text_model_name)
+    feature_extractor = get_feature_extractor(CFG.image_model_name)
+
+    dataloader_train = get_local_dataloader(dataset=CFG.dataset,tokenizer=tokenizer,feature_extractor=feature_extractor,batch_size=CFG.batch_size,shuffle=CFG.shuffle_train,split="train")
+    dataloader_valid = get_local_dataloader(dataset=CFG.dataset,tokenizer=tokenizer,feature_extractor=feature_extractor,batch_size=CFG.batch_size,shuffle=CFG.shuffle_train,split="val")
+
+ 
+    number_of_step_per_epoch = len(dataloader_train)
+    
+    print("prepare the model")
+    mem_before_model = torch.cuda.memory_allocated()
+    loss_fn = CLIPMoCOLoss().to(device)
+    model = CLIPMoco()
+    # copy the pruned weights of the main text to the side LST text network
+    
+    
+    if CFG.side_text_weights_copy or CFG.side_image_weights_copy:
+        print("Starting copying the weights to the pruned side network")
+        # Create a simple model without the ladder to computer the fisher, then write the new over
+        importance_measure = compute_fisher(model, get_local_dataloader(dataset="flickr30k",tokenizer=tokenizer,feature_extractor=feature_extractor,batch_size=1,shuffle=CFG.shuffle_train,split="train"), num_samples=CFG.samples_for_fisher)
+        print("fisher importance measure computed")
+
+        # Copy the pruned weights
+        model = modify_model_after_init(model,tokenizer,feature_extractor,importance_measure)
+        print("Finish copying the weights to the pruned side network")
+        importance_measure = None
+
+    
+        
+    model.to(device)
+
+    resume_model(model)
+
+    text_tower_trainable_params = sum(p.numel() for p in model.text_encoder.parameters() if p.requires_grad)
+    text_tower_total_params = sum(p.numel() for p in model.text_encoder.parameters())
+    text_head_trainable_params = sum(p.numel() for p in model.text_projection.parameters() if p.requires_grad)
+    text_head_total_params = sum(p.numel() for p in model.text_projection.parameters())
+    image_tower_trainable_params = sum(p.numel() for p in model.image_encoder.parameters() if p.requires_grad)
+    image_tower_total_params = sum(p.numel() for p in model.image_encoder.parameters())
+    image_head_trainable_params = sum(p.numel() for p in model.image_projection.parameters() if p.requires_grad)
+    image_head_total_params = sum(p.numel() for p in model.image_projection.parameters())
+
+    data_to_save.append(text_tower_total_params)
+    data_to_save.append(text_head_total_params)
+    data_to_save.append(image_tower_total_params)
+    data_to_save.append(image_head_total_params)
+    data_to_save.append(text_tower_trainable_params)
+    data_to_save.append(text_head_trainable_params)
+    data_to_save.append(image_tower_trainable_params)
+    data_to_save.append(image_head_trainable_params)
+    
+    # wrap the model with DDP
+    # device_ids tell DDP where is your model
+    # output_device tells DDP where to output, in our case, it is rank
+    # find_unused_parameters=True instructs DDP to find unused output of the forward() function of any module in the model
+    data_to_save.append(mem_before_model)
+
+    memory_after_model = torch.cuda.memory_allocated() 
+    data_to_save.append(memory_after_model)
+    
+    #Parameter
+    params = []
+    if CFG.text_backbone_finetune:
+        #params.append({"params" : [p for n,p in model.module.text_encoder.named_parameters() if (("side_encoder" in n) or ("downsampler" in n)) and (p.requires_grad)], "lr" : CFG.text_encoder_lr})
+        #params.append({"params" : [p for n,p in model.module.text_encoder.named_parameters() if ("side_encoder" not in n) and ("downsampler" not in n) and (p.requires_grad)], "lr" : CFG.text_head_lr})
+        params.append({"params" : model.text_encoder.parameters(), "lr" : CFG.text_encoder_lr})
+    if CFG.image_backbone_finetune:
+        #params.append({"params" : [p for n,p in model.module.image_encoder.named_parameters() if (("side_encoder" in n) or ("downsampler" in n)) and (p.requires_grad)], "lr" : CFG.image_encoder_lr})
+        #params.append({"params" : [p for n,p in model.module.image_encoder.named_parameters() if ("side_encoder" not in n) and ("downsampler" not in n) and (p.requires_grad)], "lr" : CFG.image_head_lr})
+        params.append({"params" : model.image_encoder.parameters(), "lr" : CFG.image_encoder_lr})
+    params.append({"params" : model.text_projection.parameters(), "lr" : CFG.text_head_lr})
+    params.append({"params" : model.image_projection.parameters(), "lr" : CFG.image_head_lr})
+
+
+    #Optimizer
+    optimizer = torch.optim.AdamW(params, weight_decay=0.)
+    #Learning rate
+    lr_scheduler = get_cosine_schedule_with_warmup(optimizer,num_warmup_steps=CFG.warming_epochs*number_of_step_per_epoch,num_training_steps=CFG.epochs*number_of_step_per_epoch)
+
+
+
+    best_loss = float("inf")
+
+    
+    for epoch in range(CFG.epochs):
+       
+        print(f"Epoch: {epoch + 1}")
+        ## TRAINING
+        model.train()
+    
+        
+
+
+        train_loss,data_returned = train_one_epoch(model, loss_fn, dataloader_train, optimizer,lr_scheduler,device)
+        ## VALIDATION
+        model.eval()
+
+
+        for item in data_returned:
+            data_to_save.append(item)
+        #with torch.no_grad():
+        #    valid_loss = valid_one_epoch(model,loss_fn,dataloader_valid,device)
+
+
+
+        break
+
+
+        
+            
+        
+       
+        #lr_scheduler.step()
+
+    with open("memory.csv", 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(data_to_save)
+    print("Finish training")
+    cleanup()
+
+
+
+
+if __name__ == "__main__":
+
+    
+    # world_size is the number of GPU available
+    world_size = CFG.gpu_number   
+    mp.spawn(
+        main,
+        args=(world_size,),
+        nprocs=world_size
+    )
---
+++ training_for_memory.py
@@ -0,0 +1,161 @@
+from tqdm import tqdm
+import config as CFG
+import wandb
+import torch
+
+class AvgMeter:
+    def __init__(self, name="Metric"):
+        self.name = name
+        self.reset()
+
+    def reset(self):
+        self.avg_loss,self.sum_loss, self.count = [0] * 3
+
+    def update(self, loss, count=1):
+        self.count += count
+        self.sum_loss += loss * count
+        self.avg_loss = self.sum_loss / self.count
+        
+
+    def __repr__(self):
+        text = f"{self.name}: avg_loss = {self.avg_loss:.4f}"
+        return text
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group["lr"]
+
+data_to_return = []
+
+def train_one_epoch(model, loss_fn, train_loader, optimizer,lr_scheduler,device):
+    
+    loss_meter = AvgMeter()
+    # For cc3m, using local dataloader with hardcoded fixes for now
+    tqdm_object = tqdm(train_loader, total=len(train_loader))
+    
+    for batch in tqdm_object:
+    #for batch in train_loader:
+
+        a = torch.cuda.memory_allocated(device)
+
+
+        image = batch["image"].to(device)
+        text = {"input_ids": batch["input_ids"].to(device), "attention_mask": batch["attention_mask"].to(device)}
+        
+        # Generate key for this batch, and update the queue outside of the forward pass
+        with torch.no_grad():
+           
+            # Update the momentum encoder
+            model._momentum_update_key_encoders()
+
+            # Compute the keys
+            key_image_features = model.key_encode_image(image).to(device)
+            key_text_features = model.key_encode_text(text).to(device)
+
+            # Get the queue 
+            key_image_from_queue = model.image_queue.clone().detach().to(device)
+            key_text_from_queue = model.text_queue.clone().detach().to(device)
+
+
+            # Now the keys are the cat of new and stored queue
+            keys_for_this_batch = {"image_embed" : torch.cat([key_image_features, key_image_from_queue], dim=0), "text_embed": torch.cat([key_text_features, key_text_from_queue], dim=0)}
+       
+        
+        
+        # Zero your gradients for every batch!
+        optimizer.zero_grad()
+        
+        #compute prediction for the batch
+        output = model(image,text)
+        
+        b = torch.cuda.memory_allocated(device)
+
+        data_to_return.append(b)
+        #print(f"2 - After forward pass: {b}")
+        #print(f"2 - Memory consumed by forward pass: {b-a}")
+        #compute loss and its gradients
+        loss = loss_fn(output,keys_for_this_batch)
+
+        c = torch.cuda.memory_allocated(device)
+        data_to_return.append(c)
+        #print(f"3 - After computing loss and its gradient pass: {c}")
+        loss.backward()
+
+        d = torch.cuda.memory_allocated(device)
+        data_to_return.append(d)
+        #print(f"4 - After loss backward pass: {d}")
+        # Adjust learning weights
+        optimizer.step()
+
+        e = torch.cuda.memory_allocated(device)
+        data_to_return.append(e)
+        #print(f"5 - After optimizer pass: {e}")
+        lr_scheduler.step()  # Update learning rate
+        
+        # Dequeue and enqueue the new keys
+        with torch.no_grad():
+            
+            model._dequeue_and_enqueue(key_image_features,key_text_features)
+
+        # Gather data and report
+        count = batch["image"].size(0)
+        loss_meter.update(loss, count)
+
+        #tqdm_object.set_postfix(train_loss=loss_meter.avg_loss.item())
+
+        break
+        
+        
+    return loss_meter,data_to_return
+
+
+
+################## VALIDATION EPOCH #################################
+
+
+
+def valid_one_epoch(model,loss_fn,valid_loader,device):
+
+    loss_meter = AvgMeter()
+
+    #tqdm_object = tqdm(valid_loader, total=len(valid_loader))
+    counter = 0
+
+    #for batch in tqdm_object:
+    for batch in valid_loader:
+        counter += 1
+        if counter >= 140:
+            break
+        print(f"Val Minibatch: {counter}", end="\r", flush=True)
+        image = batch["image"].to(device)
+        text = {"input_ids": batch["input_ids"].to(device), "attention_mask": batch["attention_mask"].to(device)}
+
+        # Generate key for this batch, but don't update queue nor momentum update 
+        with torch.no_grad():
+            
+            # Compute the keys
+            key_image_features = model.module.key_encode_image(image).to(device)
+            key_text_features = model.module.key_encode_text(text).to(device)
+
+            # Get the queue 
+            key_image_from_queue = model.module.image_queue.clone().detach().to(device)
+            key_text_from_queue = model.module.text_queue.clone().detach().to(device)
+
+            # Now the keys are the cat of new and stored queue
+            keys_for_this_batch = {"image_embed" : torch.cat([key_image_features, key_image_from_queue], dim=0), "text_embed": torch.cat([key_text_features, key_text_from_queue], dim=0)}
+            
+        #compute prediction for the batch
+        output = model(image,text)
+        
+        #compute loss and its gradients
+        loss = loss_fn(output,keys_for_this_batch)
+
+        # Gather data and report
+        count = batch["image"].size(0)
+        loss_meter.update(loss, count)
+
+        wandb.log({"step Validation loss": loss_meter.avg_loss} )
+        #tqdm_object.set_postfix(valid_loss=loss_meter.avg_loss.item())
+
+    return loss_meter
+
