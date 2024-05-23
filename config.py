431852404a06a694ebf99dab9dfeb07df03d2063
CarrelV <vin100.c@hotmail.com>
2024-05-23 23:19:34 +0200

    memory benchmark LiT

--- config.py
+++ config.py
@@ -11,7 +11,7 @@
 #             Training         #
 ################################
 
-text_model_size = "small"
+text_model_size = "medium"
 image_model_size = "medium"
 
 ## See at the end the different possibilities
@@ -56,7 +56,7 @@ sum_last_outputs = True
 
 configuration_to_test = "lora"
 
-run_info = "classic_LiT (I=medium/T=small)"
+run_info = "classic_LiT (I=medium/T=medium)"
 training_dataset = "mscoco"
 # best or im0 or i2t or t2i
 weight_information = "best"
