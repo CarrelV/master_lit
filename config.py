98d45d5874f6563d5c35a7d60ad426abf974685c
CarrelV <vin100.c@hotmail.com>
2024-05-23 23:11:26 +0200

    memory benchmark APE

--- config.py
+++ config.py
@@ -11,7 +11,7 @@
 #             Training         #
 ################################
 
-text_model_size = "small"
+text_model_size = "medium"
 image_model_size = "small"
 
 ## See at the end the different possibilities
@@ -56,7 +56,7 @@ sum_last_outputs = True
 
 configuration_to_test = "lora"
 
-run_info = "APE (I=small/T=small)"
+run_info = "APE (I=small/T=medium)"
 training_dataset = "mscoco"
 # best or im0 or i2t or t2i
 weight_information = "best"
